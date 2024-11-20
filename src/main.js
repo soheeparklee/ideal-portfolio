bcd20457fed6ac959752bda64be551c7e2eb10a2
soheeparklee <soheeparklee@gmail.com>
2024-11-20 14:45:01 +0100

    JS_nav bar toggle

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@
 //dark color background when scrolled down
 const header = document.querySelector('.header');
 const headerHeight= header.getBoundingClientRect().height;
+//arrow
+//show when scolled down
+const arrow= document.querySelector('.arrow_up')
+const arrowHeight= arrow.offsetHeight;
 document.addEventListener('scroll', () => {
     if( window.scrollY > headerHeight){
         header.classList.add('header--dark');
+        arrow.classList.add('arrow--show');
     }else{
         header.classList.remove('header--dark');
+        arrow.classList.remove('arrow--show');
     }
 })
 
@@ -15,6 +21,17 @@ document.addEventListener('scroll', () => {
 const home= document.querySelector('.home__container');
 const homeHeight= home.offsetHeight;
 document.addEventListener('scroll', ()=> {
-    console.log(1 - window.scrollY / homeHeight);
     home.style.opacity= 1 - window.scrollY / homeHeight;
+})
+
+
+//navbar button toggle
+const navbarMenu= document.querySelector('.header__menu');
+const navbarToggle= document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('open');
+})
+//navbar button close
+navbarMenu.addEventListener('click', ()=>{
+    navbarMenu.classList.remove('open');
 })
