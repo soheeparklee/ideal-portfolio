5e97fc9ad2d51c9b8a50dbb819c6067135103d3b
soheeparklee <soheeparklee@gmail.com>
2024-11-21 01:03:43 +0100

    JS_nav bar intersection observer

--- src/project.js
+++ src/project.js
@@ -1,2 +1,22 @@
 'use scrict';
 
+const skillContainer= document.getElementById('skills');
+const skillsBtn= document.querySelector('a[href="#skills"]');
+
+function handleIntersection(entries) {
+    // The callback will return an array of entries, even if you are only observing a single item
+    entries.map((entry) => {
+    if (entry.isIntersecting) {
+        skillsBtn.classList.add('visible')
+    } else {
+        skillsBtn.classList.remove('visible')
+    }
+    });
+}
+
+// Next we instantiate the observer with the function we created above. This takes an optional configuration
+// object that we will use in the other examples.
+const observer = new IntersectionObserver(handleIntersection);
+
+// Finally start observing the target element
+observer.observe(skillContainer);
